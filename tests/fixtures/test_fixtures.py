f9f6c025a2d781a846aa73747b002b5d5a6ecde2
Julian Torres <julian.torres@mongodb.com>
2020-12-03 13:14:29 -0500

    Adjust naming (shouldn't affect outcome)

--- tests/fixtures/test_fixtures.py
+++ tests/fixtures/test_fixtures.py
@@ -1,6 +1,6 @@
 import pytest
 
 
-@pytest.fixture(name="some_fixture")
+@pytest.fixture(name="mock_fixture")
 def mock_fixture(mocker):
     mocker.patch("app.return_data", return_value=["fixture data"])
--- tests/test_pylance.py
+++ tests/test_pylance.py
@@ -1,7 +1,7 @@
 import app
 
 
-def test_pylance_with_fixtures(some_fixture):
+def test_pylance_with_fixtures(mock_fixture):
     # Note that this is NOT "some data", and passes, but Pylance claims "some_fixture" is not accessed
     expected = ["fixture data"]
     actual = app.return_data()
