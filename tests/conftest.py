c3c0ce5262e7bc5e19e015e0e9f9756f94c37cba
Julian Torres <julian.torres@mongodb.com>
2020-12-03 14:56:33 -0500

    Fixing things based on feedback

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import pytest
 from fixtures.test_fixtures import mock_fixture
 
+# FIX: Add "python.analysis.extraPaths": ["tests"] to settings
 # fixtures.test_fixtures says it cannot be resolved, however it properly _is_ resolved, because the tests run fine
